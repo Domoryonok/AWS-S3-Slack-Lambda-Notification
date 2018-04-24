0551445fac5b4c6e65cc5403a0ca6805c0df5fa2
Alexander Domorenok <alexdomoryonok@gmail.com>
2018-04-24 11:40:47 +0300

    added better error logger

--- utils/errorHandler.go
+++ utils/errorHandler.go
@@ -1,9 +1,21 @@
 package utils
 
-import "log"
+import (
+	"log"
+	"runtime"
+	"strings"
+)
+
+func getReadebleLogPath(path string) string {
+	var splitPath []string =  strings.Split(path, "/")
+	return strings.Join(splitPath[len(splitPath) - 3:], "/")
+}
 
 func HandleError(err error, reason string) {
 	if err != nil {
-		log.Fatalf("%s error: %v", reason, err)
+		_, file, line, _ := runtime.Caller(1)
+		file = getReadebleLogPath(file)
+
+		log.Fatalf("[.../%s:%d] - %s error : %v", file, line, reason, err)
 	}
 }
